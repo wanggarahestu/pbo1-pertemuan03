8c6b5409cf7a1c567e48a80e568ceff50578921a
Wangga Rahestu <62321623+wanggarahestu@users.noreply.github.com>
2020-05-02 23:30:37 +0800

    percabangan Switch

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,27 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalan=3;
+        switch (jalanJalan){
+            case 1:
+                System.out.println("Urang Lapas");
+                break;
+            case 2:
+                System.out.println("Urang Acayaan");
+                break;
+            case 3:
+                System.out.println("Ubat Nyamuk");
+                break;
+            case 4:
+                System.out.println("Dubil Dit");
+                break;
+                default:
+                    System.out.println("Mahluk Halus");
+                    break;
+
+
+        }
+    }
+
+}
