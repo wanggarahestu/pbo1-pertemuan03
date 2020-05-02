7e7fd44d6883b95bb64d2f617c28e256f9a20463
Wangga Rahestu <62321623+wanggarahestu@users.noreply.github.com>
2020-05-03 00:46:26 +0800

    Modifikasi pernyataan 1 tampil

--- src/percabangan/ModifikasiPercabangan.java
+++ src/percabangan/ModifikasiPercabangan.java
@@ -2,6 +2,24 @@ package percabangan;
 
 public class ModifikasiPercabangan {
     public static void main(String[] args) {
+        boolean taatAturan = true;
+        char nama='B';
+
+
+        switch (nama){
+            case 'A':
+                System.out.println("Arif Putra, Tidak pernah membawa Kelengkapan Surat berkendara");
+                break;
+            default:
+                System.out.println("Wangga Rahestu,Selalu membawa Surat STNK saat berkendara ");
+                break;
+        }
+        if (taatAturan) {
+            System.out.println("Maka Tidak Akan di tilang");  //true
+
+        }else {
+            System.out.println("Karena lalai maka akan kena sanksi oleh Polisi"); // false
+        }
 
     }
 }
