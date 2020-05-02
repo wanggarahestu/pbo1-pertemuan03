61a9176f1c220bf8190a1768f04ce17d9cef629e
Wangga Rahestu <62321623+wanggarahestu@users.noreply.github.com>
2020-05-02 23:03:51 +0800

    IfElse Pernyataan 2 yang tampil

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,14 +2,14 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 53000;
-        double uangDidompet = 25000;
+        double totalBelanja = 25000;
+        double uangDidompet = 50000;
 
         if (uangDidompet<totalBelanja) {
             System.out.println("Uang kurang, kurangi jajan anda");
 
         }else {
-            System.out.println("Uang cukup, Selamat Menikmati");
+            System.out.println("Uang cukup, berelaan lah");
         }
     }
 
