cc64d098e83fc220288990b3674a5d44d3bf70f2
Wangga Rahestu <62321623+wanggarahestu@users.noreply.github.com>
2020-05-02 21:12:26 +0800

    substring(0,4) untuk menampilkan 5 huruf diawal

--- src/string/OperasiString.java
+++ src/string/OperasiString.java
@@ -15,5 +15,6 @@ public class OperasiString {
         System.out.println(kota.toUpperCase());
         System.out.println(kota.toLowerCase());
         System.out.println(kota.substring(5));
+        System.out.println(kota.substring(0,4));
     }
 }
