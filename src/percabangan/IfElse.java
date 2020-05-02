9de9ec2de8af8349114c907ce7791b2d6c30592b
Wangga Rahestu <62321623+wanggarahestu@users.noreply.github.com>
2020-05-02 23:01:05 +0800

    IfElse pernyataan 1 yang jalan

---
+++ src/percabangan/IfElse.java
@@ -0,0 +1,16 @@
+package percabangan;
+
+public class IfElse {
+    public static void main(String[] args) {
+        double totalBelanja = 53000;
+        double uangDidompet = 25000;
+
+        if (uangDidompet<totalBelanja) {
+            System.out.println("Uang kurang, kurangi jajan anda");
+
+        }else {
+            System.out.println("Uang cukup, Selamat Menikmati");
+        }
+    }
+
+}
