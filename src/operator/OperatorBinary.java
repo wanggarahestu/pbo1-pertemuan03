d2ccbee9f7be7eadf5269d8f083315c083850989
Wangga Rahestu <62321623+wanggarahestu@users.noreply.github.com>
2020-05-02 21:49:59 +0800

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -2,7 +2,24 @@ package operator;
 
 public class OperatorBinary {
 
+    //~~sengaja Di Catat siapa Tau nanti lupa ~~
+    //(sout) untuk menampilkan system out println secara otomatis
+    //(psvm) menampilkan otomatis static public main
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/ komentar yang tidak dijalankan oleh program
+
+
     String nama = "Wangga Rahestu";
     int jumlahSks = 24;
     double indeksPrestasiKumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
