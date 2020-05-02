5f48e7fb7aefcc7fe9ecb18a5384cbf6f4bd6163
Wangga Rahestu <62321623+wanggarahestu@users.noreply.github.com>
2020-05-02 23:17:15 +0800

    IfElse 3 Kemungkinan Pernyataan 3 jalan

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -3,7 +3,7 @@ package percabangan;
 public class IfElse {
     public static void main(String[] args) {
         double totalBelanja = 25000;
-        double uangDidompet = 50000;
+        double uangDidompet = 25000;
 
         if (uangDidompet<totalBelanja) {
             System.out.println("Uang kurang, kurangi jajan anda");
